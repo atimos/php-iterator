4b9edb665ce1a16b85f976af7995aeee60308082
Alexander Seppälä <dev@timot.se>
2019-09-23 23:55:12 +0200

    add phan

---
+++ .phan/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'target_php_version' => null,
+    'directory_list' => ['src'],
+    'exclude_file_regex' => '@^vendor/.*/(tests?|Tests?)/@',
+    'exclude_analysis_directory_list' => ['vendor/'],
+];
