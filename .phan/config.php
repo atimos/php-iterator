019bf25eb7de517cbdf9c86bb77fb80fa8cbff3b
Timo <dev@timot.se>
2020-02-03 04:56:53 +0100

    remove phan config

--- .phan/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'target_php_version' => null,
-    'directory_list' => ['src'],
-    'exclude_file_regex' => '@^vendor/.*/(tests?|Tests?)/@',
-    'exclude_analysis_directory_list' => ['vendor/'],
-];
